46aa3eae0023328a7566abd1927763483b5a07a4
Lavish Garg <lgarg_be19@thapar.edu>
2022-05-14 15:55:28 +0530

    yelpcamp basic styling done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override')
 const Campground = require('./models/Campground')
 
@@ -18,7 +19,7 @@ db.once("open", ()=> {
 
 const app = express(); 
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')) 
 
@@ -41,6 +42,7 @@ app.get('/campgrounds/new', async(req, res) =>{
 app.post('/campgrounds', async(req, res) =>{
     const campground = new Campground(req.body.campground);
     await campground.save();
+
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
--- seeds/index.js
+++ seeds/index.js
@@ -24,10 +24,14 @@ const seedDB = async () => {
     await Campground.deleteMany( {} );
     for(let i=0; i<100; i++){
         const random1000 = Math.floor(Math.random()*406);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground(
             {
                 location: `${cities[random1000].city}, ${cities[random1000].admin_name}`,
-                title: `${descriptors[Math.floor(Math.random()*18)]} ${places[Math.floor(Math.random()*18)]}`
+                title: `${descriptors[Math.floor(Math.random()*18)]} ${places[Math.floor(Math.random()*18)]}`,
+                image: 'https://source.unsplash.com/collection/483251',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!', 
+                price
             }
         )
         await camp.save(); 
