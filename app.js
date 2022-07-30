b3281fb8af508a38f07ed264fec976d7bf982f29
Lavish Garg <lgarg_be19@thapar.edu>
2022-07-30 23:20:49 +0530

    username bug and cloudinary image content policy security issue fixed

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/dhbiouaym/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
