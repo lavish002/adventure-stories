8feaba52e5b6c78d0f6724b4ebb9fbe2877146ea
Lavish Garg <lgarg_be19@thapar.edu>
2022-07-31 15:16:41 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const helmet = require('helmet');
 // npm i express-mongo-sanitize
 app.use(mongoSanitize())   //it will not allow keywords through query strings or params like $gt
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp-final'
 
 //db connect
 mongoose.connect(dbUrl);
