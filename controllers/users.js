d2502e8b53194531654853123a13914ffbfb998b
Lavish Garg <lgarg_be19@thapar.edu>
2022-08-07 23:24:19 +0530

    logout error fixed

--- controllers/users.js
+++ controllers/users.js
@@ -35,7 +35,9 @@ module.exports.login = (req, res) => {
 }
 
 module.exports.logout = (req, res) => {
-    req.logout();
+    req.logout(function(err) {
+        if (err) { return next(err); }
     req.flash('success', `Goodbye :(`);
     res.redirect('/campgrounds');
+    })
 }
